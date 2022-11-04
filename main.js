34f4008afb12419f1241622865e81ec85268d104
Luis Mario <43504168+LuisM711@users.noreply.github.com>
2022-11-04 01:20:43 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -10,17 +10,18 @@ fn = () => {
     let x = document.getElementById("inputX").value;
     let lambda = document.getElementById("inputLambda").value;
     let output = document.getElementById("resultado");
+    console.log(x,lambda);
     if (x != Math.round(x)) {
-        output.innerHTML = ("La variable 'X' debe ser entera");window.scrollTo(0, 1000);
+        output.innerHTML = ("La variable 'X' debe ser entera");window.scrollTo(0, 10000);
         return 0;
     }
     if (x < 0 || lambda < 0) {
-        output.innerHTML = ("Las variables deben ser mayor a 0");window.scrollTo(0, 1000);
+        output.innerHTML = ("Las variables deben ser mayor a 0");window.scrollTo(0, 10000);
         return 0;
     }
     if(document.getElementById("inputX").value==""||document.getElementById("inputLambda").value=="")
     {
-        output.innerHTML = ("Debe ingresar datos");window.scrollTo(0, 1000);
+        output.innerHTML = ("Debe ingresar datos validos");window.scrollTo(0, 10000);
         return 0;
     }
     // x = Math.round(x);
@@ -28,16 +29,16 @@ fn = () => {
     let resultado = 0;
     resultado = ((Math.pow(lambda, x)) * (Math.pow(Math.E, (lambda) * -1))) / (factorial(x));
     console.log(resultado);
-    output.innerHTML = `P(X|λ) = ${resultado.toFixed(4)}`;
-    window.scrollTo(0, 1000);
+    output.innerHTML = resultado>0.0001 ? `P(X|λ) = ${resultado.toFixed(4)}`:`P(X|λ) = ${resultado}`
+    window.scrollTo(0, 10000);
 
 }
 
 borrar = () =>
 {
     console.clear();
-    document.getElementById("inputX").value = "";
-    document.getElementById("inputLambda").value = "";
+    document.getElementById("inputX").value = "0";
+    document.getElementById("inputLambda").value = "0";
     document.getElementById("resultado").innerHTML = "";
 }
 
@@ -47,7 +48,7 @@ borrar = () =>
 
 
 
-function factorial(num) {
+ factorial = (num) =>{
     if (num == 0) { return 1; }
     else { return num * factorial(num - 1); }
 }
